ce53cbdd4e610613f5ed064e3a89aadd576a84ae
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2018-01-24 15:37:24 -0200

    Aula 05 - Melhorando a experiência do usuário

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 
 export class FotoComponent {
 
-    @Input() titulo;
-    @Input() url;
+    @Input() titulo: string;
+    @Input() url: string;
+    descricao: string;
 }
